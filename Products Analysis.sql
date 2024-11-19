d1f1dbec93985d779cec28b65f8890d738a1f601
Adedamola A. Adeagbo <154677867+Dedamola1@users.noreply.github.com>
2024-11-19 17:12:07 +0100

    Update

---
+++ Products Analysis.sql
@@ -0,0 +1,160 @@
+-- Active: 1729712001528@@127.0.0.1@3306@db_schema
+USE db_schema;
+
+-- Total orders recorded for each product line
+SELECT `productLine`,
+        COUNT(`orderNumber`) AS total_Orders
+FROM products_new pn
+JOIN orderdetails od
+    ON od.`productCode` = pn.`productCode`
+GROUP BY `productLine`
+ORDER BY total_orders DESC
+;
+
+-- Total orders recorded for each product 
+SELECT `productName`,
+        COUNT(`orderNumber`) AS total_Orders
+FROM products_new pn
+JOIN orderdetails od
+    ON od.`productCode` = pn.`productCode`
+GROUP BY `productName`
+ORDER BY total_orders DESC
+;
+
+-- Quantity ordered for each product line
+SELECT `productLine`,
+        SUM(`quantityOrdered`) AS total_quantity_ordered
+FROM products_new pn
+JOIN orderdetails od
+    ON od.`productCode` = pn.`productCode`
+GROUP BY `productLine`
+ORDER BY total_quantity_ordered DESC
+;
+
+-- Quantity ordered for each product 
+SELECT `productName`,
+        SUM(`quantityOrdered`) AS total_quantity_ordered
+FROM products_new pn
+JOIN orderdetails od
+    ON od.`productCode` = pn.`productCode`
+GROUP BY `product`
+ORDER BY total_quantity_ordered DESC
+;
+
+-- Top 10 & Bottom 10 products by quantity ordered
+(SELECT `productLine`,
+        `productName`,
+        SUM(`quantityOrdered`) AS total_quantity_ordered,
+        'Top' AS position
+FROM products_new pn
+JOIN orderdetails od
+    ON od.`productCode` = pn.`productCode`
+GROUP BY `productName`, `productLine`
+ORDER BY total_quantity_ordered DESC
+LIMIT 10)
+UNION ALL
+(SELECT `productLine`,
+        `productName`,
+        SUM(`quantityOrdered`) AS total_quantity_ordered,
+        'Bottom' AS position
+FROM products_new pn
+JOIN orderdetails od
+    ON od.`productCode` = pn.`productCode`
+GROUP BY `productName`, `productLine`
+ORDER BY total_quantity_ordered ASC
+LIMIT 10)
+;
+
+-- Top product in product line by orders
+WITH product_CTE AS (
+    SELECT `productName`,
+            `productLine`,
+            SUM(`quantityOrdered`) AS total_orders,
+            ROW_NUMBER () OVER(PARTITION BY `productLine` ORDER BY SUM(`quantityOrdered`) DESC) AS row_num
+    FROM products_new pn
+    JOIN orderdetails od
+        ON od.`productCode` = pn.`productCode`
+    GROUP BY `productName`, `productLine`
+)
+SELECT `productLine`,
+        `productName`,
+        total_orders
+FROM `product_CTE`
+WHERE row_num = 1
+;
+
+-- Total orders by status
+SELECT odn.status,
+        COUNT(od.`orderNumber`) AS total_orders
+FROM products_new pn
+JOIN orderdetails od 
+    ON od.`productCode` = pn.`productCode`
+JOIN orders_new odn
+    ON odn.`orderNumber` = od.`orderNumber`
+GROUP BY odn.status
+;
+
+SELECT status,
+        COUNT(`orderNumber`) AS total_orders
+FROM orders_new 
+GROUP BY status
+;
+
+-- Total shipped orders by product line
+WITH product_CTE AS (
+    SELECT  pn.`productLine`,
+            odn.status,
+            COUNT(od.`orderNumber`) AS total_shipped_orders,
+            ROW_NUMBER () OVER(PARTITION BY odn.status ORDER BY COUNT(od.`orderNumber`) DESC) AS row_num
+    FROM products_new pn
+    JOIN orderdetails od
+        ON od.`productCode` = pn.`productCode`
+    JOIN orders_new odn
+        ON odn.`orderNumber` = od.`orderNumber`
+    GROUP BY pn.`productLine`, odn.status
+    HAVING odn.status = 'Shipped'
+)
+SELECT `productLine`,
+        total_shipped_orders
+FROM `product_CTE`
+;
+
+-- Total shipped orders by product
+WITH product_CTE AS (
+    SELECT  pn.`productName`,
+            odn.status,
+            COUNT(od.`orderNumber`) AS total_shipped_orders,
+            ROW_NUMBER () OVER(PARTITION BY odn.status ORDER BY COUNT(od.`orderNumber`) DESC) AS row_num
+    FROM products_new pn
+    JOIN orderdetails od
+        ON od.`productCode` = pn.`productCode`
+    JOIN orders_new odn
+        ON odn.`orderNumber` = od.`orderNumber`
+    GROUP BY pn.`productName`, odn.status
+    HAVING odn.status = 'Shipped'
+)
+SELECT `productName`,
+        total_shipped_orders
+FROM `product_CTE`
+;
+
+-- Total shipped orders by counntry 
+WITH product_CTE AS (
+    SELECT c.country,
+            odn.status,
+            COUNT(od.`orderNumber`) AS total_shipped_orders,
+            ROW_NUMBER () OVER(PARTITION BY odn.status ORDER BY COUNT(od.`orderNumber`) DESC) AS row_num
+    FROM products_new pn
+    JOIN orderdetails od
+        ON od.`productCode` = pn.`productCode`
+    JOIN orders_new odn
+        ON odn.`orderNumber` = od.`orderNumber`
+    JOIN customers_new c 
+        ON c.`customerNumber` = odn.`customerNumber`  
+    GROUP BY c.country, odn.status
+    HAVING odn.status = 'Shipped'
+)
+SELECT country,
+        total_shipped_orders
+FROM `product_CTE`
+;
---
+++ Stores Data Cleaning.sql
@@ -0,0 +1,157 @@
+-- Active: 1729712001528@@127.0.0.1@3306@db_schema
+USE db_schema;
+
+-- Verifying MYSQL queries work
+ALTER TABLE customers_new
+ADD product_count INT;
+
+ALTER TABLE customers_new
+DROP COLUMN product_count;
+
+SELECT *
+FROM customers;
+
+-- Duplicating customers table for cleaning
+CREATE TABLE customers_new
+LIKE customers;
+
+SELECT *
+FROM customers_new;
+
+INSERT customers_new
+SELECT *
+FROM customers;
+
+-- Duplicating offices table for cleaning
+CREATE TABLE offices_new
+LIKE offices;
+
+SELECT *
+FROM offices_new;
+
+INSERT offices_new
+SELECT *
+FROM offices;
+
+-- Duplicating orders table for cleaning
+CREATE TABLE orders_new
+LIKE orders;
+
+SELECT *
+FROM orders_new;
+
+INSERT orders_new
+SELECT *
+FROM orders;
+
+-- Duplicating productlines table for cleaning
+CREATE TABLE productlines_new
+LIKE productlines;
+
+SELECT *
+FROM productlines_new;
+
+INSERT productlines_new
+SELECT *
+FROM productlines;
+
+-- Duplicating productlines table for cleaning
+CREATE TABLE products_new
+LIKE products;
+
+SELECT *
+FROM products_new;
+
+INSERT products_new
+SELECT *
+FROM products;
+
+-- Duplicating employees table for cleaning
+CREATE TABLE employees_new
+LIKE employees;
+
+SELECT *
+FROM employees_new;
+
+INSERT employees_new
+SELECT *
+FROM employees;
+
+
+-- Cleaning new customers table
+ALTER TABLE customers_new
+DROP COLUMN state,
+DROP COLUMN phone,
+DROP COLUMN addressline2;
+
+-- Cleaning new offices table
+ALTER TABLE offices_new
+DROP COLUMN addressline2,
+DROP COLUMN state;
+
+UPDATE offices_new
+SET territory = 'APAC'
+WHERE country = 'Japan';
+
+UPDATE offices_new
+SET territory = 'North America'
+WHERE country = 'USA';
+
+-- Cleaning new orders table
+ALTER TABLE orders_new
+DROP COLUMN comments;
+
+-- Cleaning new productlines table
+ALTER TABLE productlines_new
+DROP COLUMN htmldescription,
+DROP COLUMN image;
+
+-- Cleaning new products table
+ALTER TABLE products_new
+DROP COLUMN productdescription;
+
+-- Cleaning new employees table
+UPDATE employees_new
+SET `jobTitle` = 'Sales Manager (EMEA)'
+WHERE `employeeNumber` = 1102;
+
+-- Removing Duplicates within Tables
+-- Customers table
+WITH customers_CTE AS (
+    SELECT *,
+        ROW_NUMBER() OVER(PARTITION BY customerNumber, customerName, contactFirstName, contactLastName) AS Row_Num
+    FROM customers_new
+)
+SELECT *
+FROM customers_CTE
+WHERE Row_Num > 1;
+
+-- Orderdetails table
+WITH orderdetails_CTE AS (
+    SELECT *,
+        ROW_NUMBER() OVER(PARTITION BY orderNumber, productCode, orderLineNumber) AS Row_Num
+    FROM orderdetails
+)
+SELECT *
+FROM orderdetails_CTE
+WHERE Row_Num > 1;
+
+-- Orders table
+WITH orders_CTE AS (
+    SELECT *,
+        ROW_NUMBER() OVER(PARTITION BY orderNumber, customerNumber) AS Row_Num
+    FROM orders_new
+)
+SELECT *
+FROM orders_CTE
+WHERE Row_Num > 1;
+
+-- Payments table
+WITH payments_CTE AS (
+    SELECT *,
+        ROW_NUMBER() OVER(PARTITION BY customerNumber, checkNumber) AS Row_Num
+    FROM payments
+)
+SELECT *
+FROM payments_CTE
+WHERE Row_Num > 1;
